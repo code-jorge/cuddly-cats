ae8d188057d830d7e5f1cb83ec4d87918e0a87ec
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2025-10-28 18:19:05 +0100

    Fix

--- serverless/utils/log.js
+++ serverless/utils/log.js
@@ -1,5 +1,3 @@
 export const log = (message)=> {
-  const date = new Date().toLocaleDateString('en-GB')
-  const time = new Date().toLocaleTimeString('en-GB', { hour12: false })
-  console.log(`[LOG][${date} ${time}]: ${message}`)
+  console.log(`[LOG]: ${message}`)
 }
